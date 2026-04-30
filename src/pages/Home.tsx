5ce70f1156f643289e1ce37bc54bff654b1f62ac
lgw824949-collab <lgw824949@gmail.com>
2026-04-30 11:21:22 +0900

    JSX 문법 오류 수정 및 롤링 시스템 안정화

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -796,7 +796,6 @@ const HomePage = ({
                           );
                         })()}
                       </div>
-                      </div>
                     </section>
                   );
                 });
